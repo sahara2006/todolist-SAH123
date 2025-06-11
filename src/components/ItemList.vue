c442a04289d8fee4a225b53c3cab7b72dfa02e67
SAH123 <shota.himeji@m.ieice.org>
2025-06-11 18:51:02 +0900

    ひょうじ

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -13,7 +13,15 @@ const items = ref<Item[]>([
 </script>
 
 <template>
-  <div>ItemList</div>
+  <div>
+    <div>ItemList</div>
+    <ul>
+      <li v-for="item in items" :key="item.name">
+        <div>名前: {{ item.name }}</div>
+        <div>{{ item.price }} 円</div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style></style>
